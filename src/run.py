ea49178b3c61a4e4632c69085237aa00573f7977
Jiyang Bai <61064463+jiyangbai@users.noreply.github.com>
2023-01-03 11:13:33 -0500

    Update run.py

--- src/run.py
+++ src/run.py
@@ -9,14 +9,14 @@ def main():
 
     parser = argparse.ArgumentParser(description="Run NewPolicy.")
     parser.add_argument("--dataset", nargs="?", default="in-2004", help="Dataset name")
-    parser.add_argument("--counts", type=int, default=100, help="Number of iterations of node-mergings. Default is 100.")
-    parser.add_argument("--filters1", type=int, default=64, help="Filters (neurons) in 1st convolution. Default is 64.")
-    parser.add_argument("--filters2", type=int, default=32, help="Filters (neurons) in 2nd convolution. Default is 32.")
-    parser.add_argument("--batch_size", type=int, default=128, help="Batch size. Default is 128.")
-    parser.add_argument("--lr", type=float, default=0.001, help="Learning rate. Default is 0.001.")
-    parser.add_argument("--dropout", type=float, default=0.0, help="Dropout probability. Default is 0.0.")
-    parser.add_argument("--weight-decay", type=float, default=0.0, help="Adam weight decay. Default is 0.0.")
-    parser.add_argument("--bad_counter", type=int, default=1, help="Value of bad counts to tolerate. Default is 5.")
+    parser.add_argument("--counts", type=int, default=100, help="Number of iterations of node-mergings.")
+    parser.add_argument("--filters1", type=int, default=64, help="Filters (neurons) in 1st convolution.")
+    parser.add_argument("--filters2", type=int, default=32, help="Filters (neurons) in 2nd convolution.")
+    parser.add_argument("--batch_size", type=int, default=128, help="Batch size.")
+    parser.add_argument("--lr", type=float, default=0.001, help="Learning rate.")
+    parser.add_argument("--dropout", type=float, default=0.0, help="Dropout probability.")
+    parser.add_argument("--weight-decay", type=float, default=0.0, help="Adam weight decay.")
+    parser.add_argument("--bad_counter", type=int, default=1, help="Value of bad counts to tolerate.")
     args = parser.parse_args()
 
 
